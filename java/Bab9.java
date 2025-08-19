c6fdd1a630b9c95c208df48e3906846f4ce52f31
caey11 <akuica64@gmail.com>
2025-08-19 09:05:59 +0700

    Add files via upload

---
+++ java/Bab9.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bab9.java;
+
+/**
+ *
+ * @author PC 33 - LAB R1
+ */
+class Bab9 {
+    
+}
---
+++ java/Bab9Java.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab9.java;
+
+/**
+ *
+ * @author PC 33 - LAB R1
+ */
+public class Bab9Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Membuat objek Bab9 (otomatis panggil constructor)
+        Bab9 obj = new Bab9();
+        System.out.println("Program dijalankan.");
+    }
+    
+}
